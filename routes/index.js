452ba11b42c4e314f26d68dd0040d4d3c85ef43e
Ezra Keddell <ezrakeddell@gmail.com>
2018-10-02 11:11:43 +1300

    add a bit of pizazz

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Woop-woop! That\'s the sound of da police!' });
 });
 
 module.exports = router;
